62457bfcf7599a50d26d62f458a4931703d9a29a
Grafiel Sitanggang <2381028@unai.edu>
2024-09-19 10:15:05 +0900

    Handson 4

---
+++ ContohMethod.java
@@ -0,0 +1,9 @@
+public class ContohMethod {
+    public static int hitungLuasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 10;
+    }
+}
---
+++ ContohOverloading.java
@@ -0,0 +1,26 @@
+public class ContohOverloading {
+    public static double hitungLuas(double sisi) {
+        return sisi * sisi;
+    }
+    public static double hitungLuas(double panjang, double lebar) {
+        return panjang * lebar;
+    }
+    public static double hitungLuas(double jarijari, boolean isLingkaran) {
+        if (isLingkaran) {
+            return Math.PI * jarijari * jarijari;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        double luasPersegi = hitungLuas(5);
+        System.out.println("Luas persegi : " + luasPersegi);
+
+        double luasPersegiPanjang = hitungLuas(4, 6);
+        System.out.println("Luas persegi panjang : " + luasPersegiPanjang);
+
+        double luasLingkaran = hitungLuas(4, true);
+        System.out.println("Luask lingkaran : " + luasLingkaran);
+    }
+}
